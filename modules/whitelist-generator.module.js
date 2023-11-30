7e21a84daa9994313b609b3aaf028bb1919017a6
Lorcann Rauzduel <contact@lorcannrauzduel.fr>
2023-11-30 17:21:49 +0100

    replace writeFileSync instend of writeFile

--- modules/whitelist-generator.module.js
+++ modules/whitelist-generator.module.js
@@ -1,4 +1,4 @@
-const { writeFile } = require("fs");
+const { writeFileSync } = require("fs");
 const keccak256 = require("keccak256");
 const { bufferToHex } = require("../utils/buffer-to-hex.util");
 
@@ -36,7 +36,7 @@ class WhitelistGenerator {
 	writeWhitelistFile(data) {
 		const metadata = JSON.stringify(data, null, 2);
 
-		writeFile(this.outputFile, metadata, (err) => {
+		writeFileSync(this.outputFile, metadata, (err) => {
 			if (err) {
 				// En cas d'erreur pendant l'écriture du fichier
 				throw err; 
